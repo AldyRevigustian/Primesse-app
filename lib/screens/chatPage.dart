1989c1231e12f6c9485ff960ece471458688ccd6
Aldy Revigustian <aldybudiasih@gmail.com>
2023-11-14 08:31:49 +0700

    remove

--- lib/screens/chatPage.dart
+++ lib/screens/chatPage.dart
@@ -65,8 +65,6 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
     return Container(
       decoration: BoxDecoration(color: Colors.white),
       child: SafeArea(
