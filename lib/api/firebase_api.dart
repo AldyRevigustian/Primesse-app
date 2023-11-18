5a53ab701930622d406891b4c1d0b8d9b06c8418
Aldy Revigustian <aldybudiasih@gmail.com>
2023-11-19 02:21:43 +0700

    Notification

---
+++ lib/api/firebase_api.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class FirebaseApi{
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+  Future<void> initNotification() async{
+    await _firebaseMessaging.requestPermission();
+
+    final fcm = await _firebaseMessaging.getToken();
+
+    print(fcm);
+  }
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,74 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:primesse_app/api/firebase_api.dart';
 import 'package:primesse_app/firebase_options.dart';
 import 'package:primesse_app/screens/homePage.dart';
 import 'package:primesse_app/utils/constant.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+  print(message.data);
+  flutterLocalNotificationsPlugin.show(
+      message.data.hashCode,
+      message.data['title'].toString(),
+      message.data['body'].toString(),
+      NotificationDetails(
+        android: AndroidNotificationDetails(channel.id, channel.name,
+            channelDescription: channel.description,
+            icon: "@drawable/ic_stat_pm"),
+      ));
+}
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title //
+  description: 'This channel is used for important notifications.',
+  importance: Importance.max,
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await FirebaseMessaging.instance.subscribeToTopic("ALL");
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  const initialzationSettingsAndroid =
+      AndroidInitializationSettings('@drawable/ic_stat_pm');
+  const initializationSettings =
+      InitializationSettings(android: initialzationSettingsAndroid);
+
+  flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    flutterLocalNotificationsPlugin.show(
+        message.data.hashCode,
+        message.data['title'].toString(),
+        message.data['body'].toString(),
+        NotificationDetails(
+          android: AndroidNotificationDetails(channel.id, channel.name,
+              channelDescription: channel.description,
+              icon: "@drawable/ic_stat_pm"
+              ),
+        ));
+  });
+
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    systemNavigationBarColor: Colors.white,
-    systemNavigationBarIconBrightness: Brightness.dark
-  ));
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark));
   runApp(MyApp());
 }
 
